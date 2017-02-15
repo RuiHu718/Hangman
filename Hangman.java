dbaf7b10a08a25a7602e569d4ee89da47484e42f
S <s>
2017-02-15 15:46:12 +0800

    {"types":"C","runs":5}

--- Hangman.java
+++ Hangman.java
@@ -31,8 +31,7 @@ public class Hangman extends ConsoleProgram {
     // chooses random word as target
     // uses simple hangmanLexicon stud for now
     private String chooseRandomWord() {
-        String word = hangmanLex.getWord(2);
-        return word;
+        return  hangmanLex.getWord(2);
     }
 
 
