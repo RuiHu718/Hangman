28cdd11a3ecfd4c71f138d51e580eab7cdeaef78
rui.hu <rui.hu6@icloud.com>
2017-02-15 16:27:01 +0800

    Adds the getting random word part and first part of setting up control structure

--- Hangman.java
+++ Hangman.java
@@ -39,7 +39,8 @@ public class Hangman extends ConsoleProgram {
             currentForm = currentForm.concat("-");
         }
 
-        println(currentForm);
+        //println(currentForm);
+        for 
     }
 
 
