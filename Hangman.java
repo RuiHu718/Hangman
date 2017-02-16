c8d2ec3b70523ab219cd125730c05a8473a94a64
S <s>
2017-02-16 10:55:41 +0800

    {"types":"C","runs":22}

--- Hangman.java
+++ Hangman.java
@@ -30,31 +30,14 @@ public class Hangman extends ConsoleProgram {
 
     //
     private void playGame() {
-        int wordLen = targetWord.length();
-        String currentForm = "";
 
-        // sets up the initial representation of the unguessed string
-        for (int i = 0; i < wordLen; i++) {
-            currentForm = currentForm.concat("-");
-        }
+        String currentForm = initCurrentForm();
+        String pickedLetter;
+        
+        while (true) {
+            pickedLetter = getUserInput();
+            println ("Your guess: " + pickedLetter);
 
-        //println(currentForm);
-        for (int j = 8; j > 0; j--) { // player has eight chances
-
-            println("You have " + j + " guesses left.");
-
-            String pickedLetter = readLine();
-            pickedLetter = pickedLetter.toUpperCase();
-            // there is a problem here, what if use type in two chars more than once?
-            if (pickedLetter.length() > 1) {
-                println("Illegal input, input should be a single letter so try again");
-                pickedLetter = readLine();
-                pickedLetter = pickedLetter.toUpperCase();
-            }
-            println("Your guess: " + pickedLetter);
-
-            // Checks whether the picked letter is part of target word
-            // Note that pickedletter is string type so have to convert to char first
             if (targetWord.indexOf(pickedLetter.charAt(0)) == -1) {
                 println("There are no " + pickedLetter + " in the word.");
                 println("The word now looks like this: " + currentForm);
@@ -62,9 +45,66 @@ public class Hangman extends ConsoleProgram {
                 
                 //currentForm = updateTarget(currentForm, pickedLetter);
             }
+
+            
         }
+
+
+        
+        // for (int j = 8; j > 0; j--) { // player has eight chances
+
+        //     println("You have " + j + " guesses left.");
+
+        //     String pickedLetter = readLine();
+        //     pickedLetter = pickedLetter.toUpperCase();
+        //     // there is a problem here, what if use type in two chars more than once?
+        //     if (pickedLetter.length() > 1) {
+        //         println("Illegal input, input should be a single letter so try again");
+        //         pickedLetter = readLine();
+        //         pickedLetter = pickedLetter.toUpperCase();
+        //     }
+        //     println("Your guess: " + pickedLetter);
+
+        //     // Checks whether the picked letter is part of target word
+        //     // Note that pickedletter is string type so have to convert to char first
+        //     if (targetWord.indexOf(pickedLetter.charAt(0)) == -1) {
+        //         println("There are no " + pickedLetter + " in the word.");
+        //         println("The word now looks like this: " + currentForm);
+        //     } else {
+                
+        //         //currentForm = updateTarget(currentForm, pickedLetter);
+        //     }
+        // }
     }
 
+
+    // gets user input of a single letter
+    // check for input errors
+    private String getUserInput() {
+        String letter = readLine();
+        if (letter.length() > 1) {
+            println("Illegal input, input should be a single letter so try again.");
+            letter = readLine();
+        }
+        return letter.toUpperCase();
+    }
+    
+
+    // sets up the initial representation of the unguessed string
+    private String initCurrentForm() {
+
+        int wordLen = targetWord.length();
+        String s = "";
+
+        for (int i = 0; i < wordLen; i++) {
+            s = s.concat("-");
+        }
+
+        return s;
+    }
+
+
+    
 
     // chooses random word as target
     // uses simple hangmanLexicon stud for now
