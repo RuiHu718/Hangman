a36cf7a77be96eabbc0fc4d6726826246e399c51
rui.hu <rui.hu6@icloud.com>
2017-02-17 16:45:18 +0800

    Adds two labels to display current and guessed words

--- Hangman.java
+++ Hangman.java
@@ -14,6 +14,7 @@ import java.awt.*;
 
 public class Hangman extends ConsoleProgram {
 
+    // need to commit
     public void init() {
         canvas = new HangmanCanvas();
         add(canvas);
--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -14,6 +14,7 @@ public class HangmanCanvas extends GCanvas {
 /** Resets the display so that only the scaffold appears */
 	public void reset() {
 
+            // need to commit
             beam = new GLine (getWidth()/2, 10, getWidth()/2 - BEAM_LENGTH, 10); // num 10 is arbitrary here, will adjust later
             add(beam);
             scaffold = new GLine(getWidth()/2 - BEAM_LENGTH, 10, getWidth()/2 - BEAM_LENGTH, 10 + SCAFFOLD_HEIGHT);
